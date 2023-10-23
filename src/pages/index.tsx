7eb6b0be1f09ff0c51e7768610235cda4652b7da
alinaderi <pro-3207@telesmart.ir>
2023-10-23 10:10:32 +0330

    edit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import Layout from '@components/layout/layout';
 import Link from 'next/link';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import Particles from 'react-particles';
 import { loadSlim } from 'tsparticles-slim';
+import Sound from 'react-sound';
+import { FaMusic } from 'react-icons/fa';
+import { MdMusicNote, MdMusicOff } from 'react-icons/md';
 
 export default function Home() {
   const particlesInit = useCallback(async (engine) => {
@@ -14,8 +17,31 @@ export default function Home() {
   const particlesLoaded = useCallback(async (container) => {
     await console.log(container);
   }, []);
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const [audio] = useState(typeof Audio !== 'undefined' && new Audio('/1.mp3'));
+
   return (
     <>
+      {isPlaying ? (
+        <MdMusicOff
+          onClick={() => {
+            audio.pause();
+            setIsPlaying(false);
+          }}
+          className="fixed text-black p-2 border-black border-2 shadow-xl rounded-full bottom-[3%] md:bottom-[5%] right-[3%] text-5xl z-50 cursor-pointer"
+        />
+      ) : (
+        <MdMusicNote
+          onClick={() => {
+            audio.play();
+            setIsPlaying(true);
+          }}
+          className="fixed text-black p-2 border-black border-2 shadow-xl rounded-full bottom-[3%] md:bottom-[5%] right-[3%] text-5xl z-50 cursor-pointer"
+        />
+      )}
+
       <Particles
         style={{ zIndex: 1 }}
         id="tsparticles"
@@ -91,30 +117,31 @@ export default function Home() {
           detectRetina: true,
         }}
       />
-      <div className="w-full  m-0 p-2 md:overflow-hidden relative md:h-[100vh]">
+
+      <div className="w-full  m-0 p-2 overflow-hidden relative md:h-[100vh]">
         <div className="w-full h-full p-0 m-0">
-          <div className="w-full h-full overflow-hidden md:h-[30vh] grid grid-cols-1 md:grid-cols-12 gap-4 py-2 opacity-20">
+          <div className="w-full h-full overflow-hidden md:h-[30vh] grid grid-cols-1 md:grid-cols-12 gap-4 py-2 opacity-40">
             <span className="  col-span-12 md:col-span-4 relative ">
-              <span className="absolute text-yellow-500 right-1/2 bottom-1/2 w-full text-center translate-x-2/4 translate-y-2/4  text-2xl md:text-3xl font-bold">
+              <span className="absolute text-yellow-500 right-1/2 bottom-1/2 w-full text-center translate-x-2/4 translate-y-2/4  text-2xl md:text-4xl font-bold">
                 LUXURY RETAIL
               </span>
               <img src="/assets/home/luxury.png" className="w-full h-full" />
             </span>
             <span className="bg-red-500 col-span-12 md:col-span-3 relative h-full rounded min-h-[120px] ">
-              <span className="absolute text-white right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4  text-2xl md:text-3xl font-bold w-full text-center">
+              <span className="absolute text-white right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4  text-2xl md:text-4xl font-bold w-full text-center">
                 FREE MARKET
               </span>
             </span>
             <span className=" col-span-12 md:col-span-5 relative h-full">
-              <span className="absolute text-slate-700 right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4  text-2xl md:text-3xl font-bold w-full text-center">
+              <span className="absolute text-slate-700 right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4  text-2xl md:text-4xl font-bold w-full text-center">
                 ECO-FRIENDLY BRANDS
               </span>
               <img src="/assets/home/eco.png" className="w-full h-full" />
             </span>
           </div>
           <div className="w-full h-full md:h-[39vh] grid grid-cols-1 gap-4 py-2 md:grid-cols-12">
-            <span className=" col-span-12 md:col-span-3  relative h-full opacity-20 ">
-              <span className="absolute text-red-500 right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4  text-2xl md:text-3xl font-bold w-full text-center">
+            <span className=" col-span-12 md:col-span-3  relative h-full opacity-40 ">
+              <span className="absolute text-red-500 right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4  text-2xl md:text-4xl font-bold w-full text-center">
                 DISCOUNTED RETAIL
               </span>
               <img
@@ -123,12 +150,12 @@ export default function Home() {
               />
             </span>
             <span className="col-span-12 md:col-span-5 h-full">
-              <div className="text-6xl font-serif text-center font-bold tracking-wider m-0 p-0">
+              <div className="text-4xl md:text-6xl font-serif text-center font-bold tracking-wider m-0 pr-0 md:pr-14 whitespace-nowrap">
                 PROJECT I
               </div>
               <div className=" grid    mt-4 grid-cols-12 gap-3 ">
-                <span className=" col-span-9 relative opacity-20 h-full">
-                  <span className="absolute text-white right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4  text-2xl md:text-3xl font-bold text-center">
+                <span className=" col-span-9 relative opacity-40 h-full">
+                  <span className="absolute text-white right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4  text-2xl md:text-4xl font-bold text-center">
                     COMMERCIAL BRANDS
                   </span>
                   <img
@@ -146,7 +173,7 @@ export default function Home() {
                     </div>
                   </Link>
 
-                  <div className="rounded-full cursor-pointer bg-orange-500  md:w-[90px] w-[80px]  md:h-[90px] h-[80px]  text-center flex justify-center items-center text-3xl text-white font-serif opacity-20">
+                  <div className="rounded-full cursor-pointer bg-orange-500  md:w-[90px] w-[80px]  md:h-[90px] h-[80px]  text-center flex justify-center items-center text-3xl text-white font-serif opacity-40">
                     F
                   </div>
                 </span>
@@ -159,7 +186,7 @@ export default function Home() {
                     textShadow:
                       '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
                   }}
-                  className="absolute  text-center text-amber-600 w-full drop-shadow-md right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4 text-2xl md:text-3xl font-bold "
+                  className="absolute  text-center text-amber-600 w-full drop-shadow-md right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4 text-2xl md:text-4xl font-bold "
                 >
                   SUSTAINABLE ART AND ARCHITECTURE
                 </span>
@@ -167,9 +194,9 @@ export default function Home() {
               </span>
             </Link>
           </div>
-          <div className="w-full  h-full md:h-[30vh] grid grid-cols-1 md:grid-cols-12 gap-4 py-2 opacity-20">
+          <div className="w-full  h-full md:h-[30vh] grid grid-cols-1 md:grid-cols-12 gap-4 py-2 opacity-40">
             <span className="col-span-12 md:col-span-3  relative">
-              <span className="absolute text-white right-1/3 bottom-1/2 translate-x-2/4 translate-y-2/4  text-2xl md:text-3xlfont-bold ">
+              <span className="absolute font-bold text-white right-1/3 bottom-1/2 translate-x-2/4 translate-y-2/4  text-2xl md:text-4xlfont-bold ">
                 LOW VALUE <br /> PRODUCTS
               </span>
               <img src="/assets/home/low.png" className="w-full h-full" />
@@ -177,7 +204,7 @@ export default function Home() {
             <span className=" col-span-12 md:col-span-5 ">
               <div className=" grid grid-cols-12 gap-3 h-full">
                 <span className="col-span-9 relative h-full">
-                  <span className="absolute text-white right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4  text-2xl md:text-3xl font-bold ">
+                  <span className="absolute text-white right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4  text-2xl md:text-4xl font-bold ">
                     GAMING
                   </span>
                   <img
@@ -196,7 +223,7 @@ export default function Home() {
               </div>
             </span>
             <span className=" col-span-12 md:col-span-4  relative">
-              <span className="absolute text-white right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4 text-2xl md:text-3xl font-bold w-full text-center">
+              <span className="absolute text-white right-1/2 bottom-1/2 translate-x-2/4 translate-y-2/4 text-2xl md:text-4xl font-bold w-full text-center">
                 ORGANIC FOOD
               </span>
               <img src="/assets/home/food.png" className="w-full h-full" />
