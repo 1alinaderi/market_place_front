8b7036974a370c29e3277971f9b0e22f1026dfd1
1alinaderi <malinaderi1@outlook.com>
2024-06-17 16:06:16 +0330

    edit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,12 @@ import Countdown from 'react-countdown'
 
 const HomePagePage = () => {
   return (
-    <div className="w-full h-[100vh] text-[40px]  flex justify-center items-center flex-col">
-        <h1 className='text-black'>Project I</h1>
+    <div style={{backgroundImage:"url(/assets/images/home/header.png)"}} className="bg-center bg-cover bg-no-repeat w-full h-[100vh] text-[40px]  flex justify-center items-center flex-col">
+        <div className='bg-red-600 text-white p-5 rounded-[15px] flex justify-center items-center flex-col gap-2'>
+        <h1 className='font-bold'>Project I</h1>
         <Countdown date={"2024-8-22"}/>
+        </div>
+
     </div>
   )
 }
