1d92cb3cf876fee69db8ea8d4d88874bc8100f7f
1alinaderi <malinaderi1@outlook.com>
2024-07-27 14:55:03 +0330

    edit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,11 +20,12 @@ export default class CustomDocument extends Document {
     return (
       <Html dir={getDirection(locale)}>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          {/* <link rel="preconnect" href="https://fonts.gstatic.com" /> */}
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Manrope:wght@600;700;800&display=swap"
             rel="stylesheet"
           />
+          
         </Head>
         <body>
           <Main />
